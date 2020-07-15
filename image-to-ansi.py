cb8ac9da371c0cbf12c9e2bf41f8ea914e5806f9
Cory Kennedy <cory@darkintel.io>
2020-07-15 10:57:19 -0500

    Updated with cowsay and python3

--- image-to-ansi.py
+++ image-to-ansi.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 This program provides the ability to convert an image file to ANSI compatible
@@ -29,12 +30,11 @@ allowing the user to run: python image-to-ansi.py <filename>
 
 __author__    = "Micah Elliott http://MicahElliott.com; Kevin Lange <k@dakko.us>; Takumi Sueda <puhitaku@gmail.com>; Torry Crass <tc.github@outlook.com>"
 __copyright__ = "Copyright (C) 2011 Micah Elliott.  All rights reserved."
-__credits__ = ["Micah Elliott", "Kevin Lange", "Takumi Sueda", "Torry Crass"]
-
+__credits__ = ["Cory Kennedy", "Micah Elliott", "Kevin Lange", "Takumi Sueda", "Torry Crass"]
+__maintainer__ = "CoryKennedy"
+__email__ = "cory@darkintel.io"
 __license__   = "WTFPL http://sam.zoy.org/wtfpl/"
-__version__   = "0.2"
-__maintainer__ = "Torry Crass"
-__email__ = "tc.github@outlook.com"
+__version__   = "0.3 - Updated for Python3" 
 __status__ = "Development"
 
 import sys
@@ -302,41 +302,47 @@ CLUT = [
 
 def print_help():
 
-    print ""
-    print 75 * "="
-    print 30 * "=" , "IMAGE to ANSI" , 30 * "="
-    print 75 * "="
+    print("")
+    print(75 * "=")
+    print("                        < Image to ASNI >\n" \
+"                                \   ^__^\n" \
+"                                 \  (23)\_______\n" \
+"                                    (__)\       )\/\/\n" \
+"                                        ||----w |\n" \
+"                                        ||     ||\n" \
+"                        Yes I know I'm an ASCII cowsay")
+    print(75 * "=")
 
-    print "\nCredits: " , __credits__[0] , "\n\t" ,  __credits__[1] , "\n\t" , __credits__[2] , "\n\t" , __credits__[3]
-    print "License: " , __license__
-    print "Version: " , __version__
-    print "Maintainer: " , __maintainer__ , " " , __email__
-    print "Status: " ,  __status__
+    print("\nCredits: " , __credits__[0] , "\n\t" ,  __credits__[1] , "\n\t" , __credits__[2] , "\n\t" , __credits__[3] , "\n\t" , __credits__[4])
+    print("License: " , __license__)
+    print("Version: " , __version__)
+    print("Maintainer: " , __maintainer__ , " " , __email__)
+    print("Status: " ,  __status__)
 
-    print "\nThis program allows you to convert a graphic file (preferably png) into\n" \
+    print("\nThis program allows you to convert a graphic file (preferably png) into\n" \
           "ANSI code that will display a rendition of the image in a console that\n" \
-          "supports ANSI color codes.\n"
+          "supports ANSI color codes.\n")
 
-    print "You need to have python, python-image, python-pillow. You can either\n" \
+    print("You need to have python, python-image, python-pillow. You can either\n" \
           "install these with your package manager (apt/yum) or install python-pip\n" \
-          "and install the necessary modules from there.\n"
+          "and install the necessary modules from there.\n")
 
-    print "See the github repository for more information (if available).\n"
+    print("See the github repository for more information (if available).\n")
 
-    print "\n### USAGE ###\n"
-    print "Standard:\n" \
-        "\tpython image-to-ansi.py <inputfile>\n"
+    print("\n### USAGE ###\n")
+    print("Standard:\n" \
+        "\tpython image-to-ansi.py <inputfile>\n")
 
-    print "Output to File:\n" \
-        "\tpython image-to-ansi.py <inputfile> > <outputfile>"
-    print "\n"
+    print("Output to File:\n" \
+        "\tpython3 image-to-ansi.py <inputfile> > <outputfile>")
+    print("\n")
 
-    print 75 * "-"
+    print(75 * "-")
 
-    print "Source: https://github.com/torrycrass/image-to-ansi"
+    print("Source: https://github.com/nodatafound/image-to-ansi")
 
-    print 75 * "-"
-    print ""
+    print(75 * "-")
+    print("")
 
 
 
@@ -369,12 +375,12 @@ if __name__ == '__main__':
 
     #Check that the user passed a single variable to the script
     if len(sys.argv) != 2:
-        print ""
-        print 30 * ">" , "ERROR" , 30 * "<"
-        print "\nError: You must pass a single option to the program."
-        print "use --help or -h for syntax assistance.\n"
-        print 67 * "^"
-        print ""
+        print("")
+        print(30 * ">" , "ERROR" , 30 * "<")
+        print("\nError: You must pass a single option to the program.")
+        print("use --help or -h for syntax assistance.\n")
+        print(67 * "^")
+        print("")
 
     #Handle a help request
     elif str(sys.argv[1]) == "--help" or str(sys.argv[1]) == "-h":
@@ -382,12 +388,12 @@ if __name__ == '__main__':
 
     #Check that the variable provided is a valid file name on the system
     elif os.path.isfile(sys.argv[1]) is not True:
-        print ""
-        print 30 * ">" , "ERROR" , 30 * "<"
-        print "\nError: This program requires an input file, the value provided\n" \
-            "is not a file. Please use a valid file name.\n"
-        print 67 * "^"
-        print ""
+        print("")
+        print(30 * ">" , "ERROR" , 30 * "<")
+        print("\nError: This program requires an input file, the value provided\n" \
+            "is not a file. Please use a valid file name.\n")
+        print(67 * "^")
+        print("")
 
     #If all other criteria are not met, process the file
     else:
